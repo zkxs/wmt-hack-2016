0ac51392f8596669249a2b330056543737ab2aab
Michael Ripley <Michael.Ripley@walmart.com>
2016-07-22 08:53:25 -0500

    Test button

--- util.js
+++ util.js
@@ -8,3 +8,19 @@ function getGetParameters() {
     });
     return qd;
 }
+
+
+function nuke(node) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+}
+
+function lookupItem(item, callback) {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        callback(req);
+    };
+    req.open("GET", "http://api.walmartlabs.com/v1/search?apiKey=dvj6prxrbf9t7pe7cqf75tfb&numItems=1&query=" + encodeURI(item), true);
+    req.send();
+}
